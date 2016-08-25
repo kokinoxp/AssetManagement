2055cfe6ea293bb7932c7400074d5df64630367e
kokinoxp <kokinoxp@gmail.com>
2016-08-25 22:51:16 +0700

    Asset_add.fxml

--- src/assetmanagement/AssetManagement.java
+++ src/assetmanagement/AssetManagement.java
@@ -19,7 +19,7 @@ public class AssetManagement extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Asset_Details.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Asset_add.fxml"));
         
         Scene scene = new Scene(root);
         
